bd0b26328df2c65e5c99bea9a4f314f91473fc0e
George Paton <georgewpaton@me.com>
2016-01-20 13:18:55 +1100

    Update to latest from anagram theme

--- anagrammer.js
+++ anagrammer.js
@@ -1,42 +1,39 @@
 // --------------------------------------
-//   Copyright 2012-2015 George Paton
+//   Copyright 2012-2016 George Paton
 //   Anagrammer.js, requires jQuery 1.11+
 // --------------------------------------
 
-// * User variables *
+// * User variables * Override before calling anagrammer()
 
 // How many seconds between rearranges
-	var arrangeRate = 8000;
+var arrangeRate = 8000;
 // How long the animation lasts
-	var animationDuration = 3000;
-	var animationSegmentDuration = 100;
+var animationDuration = 3000;
+var animationSegmentDuration = 100;
 
 // Ensure that the words are true anagrams
 // Also ensure that all the letters have images associated
-	var words = ["mellow arpeggiation", "george william paton", "a germinate polliwog"];
+var words = ["manage the ram", "anagram theme"];
 
-// Location where the images are stored, letters must be in the format "a.png" and within the first level
-	var imageLocation = "https://bf93433f972a5c5b9dcd92ff00e40c3e64683a0b.googledrive.com/host/0B-W0tG4VFD0sT2V6WWlWQVFqVXM/";
+var fileFormat = ".svg";
 
-// *Not yet implemented*
-// Set to cycle through the first two and randomly show the rest according to alternateAnagramChance
-	var alternateAnagramChance = 0.45;
+// Location where the images are stored, letters must be in the format "a.svg" and within the first level
+// Don't forget trailing slash!
+var imageLocation = "https://raw.githubusercontent.com/MellowArpeggiation/Anagrammer.js/master/letters/";
 
 // --------------------------------------
-
-//TODO: Implement random anagram cycle
 
 function arrangeAsString(nameString, animDuration) {
 	if ($("#anword").length == 0) {
 		anword = document.createElement("div");
 		anword.id = "anword";
-		
+
 		dummy = document.createElement("div");
 		dummy.id = "dummy";
-		
+
 		$("#anagram").append(anword);
 		$("#anagram").append(dummy);
-		
+
 		var spaces = 0;
 		for (var i = 0; i < nameString.length; i++) {
 			if (nameString[i] == ' ') {
@@ -45,7 +42,7 @@ function arrangeAsString(nameString, animDuration) {
 				spaces++
 			} else {
 				character = document.createElement("img");
-				character.src = imageLocation + nameString[i] + ".png";
+				character.src = imageLocation + nameString[i] + fileFormat;
 				$(character).addClass(nameString[i]);
 				$(character).addClass("anchar");
 				$(character).attr("index", i-spaces);
@@ -54,15 +51,15 @@ function arrangeAsString(nameString, animDuration) {
 		}
 	} else {
 		console.log("Rearranging as " + nameString + "...");
-		
+
 		$("#dummy").empty();
-		
+
 		$("#anword *").clone().appendTo($("#dummy"));
 		var offset = ($("#anword").offset());
 		$("#dummy").css("position", "absolute")
 			.css("left", 0)
 			.css("top", 0);
-		
+
 		var oldOffsets = new Array();
 		$("#dummy img").each(function(i) {
 			oldOffsets[i] = $($("#anword img")[i]).offset();
@@ -70,18 +67,18 @@ function arrangeAsString(nameString, animDuration) {
 				.css("left", oldOffsets[i].left)
 				.css("top", oldOffsets[i].top);
 		})
-		
+
 		$("#anword img").each(function(i) {
 			$(this).attr("index", i);
 		})
-		
+
 		var letters = $("#anword img").detach();
 		$("#anword").empty();
-		
+
 		var newOffsets = new Array();
-		
+
 		var spaces = 0;
-		
+
 		for (var i = 0; i < nameString.length; i++) {
 			if (nameString[i] == ' ') {
 				lBreak = document.createElement("br");
@@ -97,21 +94,21 @@ function arrangeAsString(nameString, animDuration) {
 				}
 			}
 		}
-		
+
 		$("#anword img").each(function(i) {
 			newOffsets[i] = $(this).offset();
 		})
-		
+
 		$("#anword").hide();
 		$("#dummy").show();
-		
+
 		// Animate here
 		var temps = new Array();
 		for (var i = 0; i < $("#dummy img").length; i++) {
 			var currentImage = $("#dummy img")[i];
 			$(currentImage).animate({
-			    left: newOffsets[$(currentImage).attr("index")].left,
-			    top: newOffsets[$(currentImage).attr("index")].top
+				left: newOffsets[$(currentImage).attr("index")].left,
+				top: newOffsets[$(currentImage).attr("index")].top
 			}, {duration: animDuration, complete: finishAnimating} );
 		}
 	}
@@ -125,51 +122,54 @@ function finishAnimating() {
 function rearrangeLoop() {
 	// Keep track of changes for timekeeping
 	var animateCounter = 0;
-	
+
 	arrangeAsString(words[1], animationDuration);
 	animateCounter++;
-	
-/*	
-	if (randomArrange(animateCounter)) {
-		animateCounter++;
-	}
-*/
-	
+
 	setTimeout(function() {arrangeAsString(words[0], animationDuration)}, arrangeRate * animateCounter);
 	animateCounter++;
-	
-/*	
-	if (randomArrange(animateCounter)) {
-		animateCounter++;
-	}
-*/
-	
+
 	setTimeout(rearrangeLoop, arrangeRate * animateCounter);
 }
 
-var preload = document.createElement("div");
-$(preload).hide();
-for (var i = 0; i < words[0].length; i++) {
-	if ((words[0])[i] == ' ') {
-		continue;
-	} else {
-		character = document.createElement("img");
-		character.src = imageLocation + (words[0])[i] + ".png";
-		$(preload).append(character);
+
+function anagrammer() {
+	var numCharacters = 0,
+		charactersLoaded = 0;
+	var preload = document.createElement("div");
+	$(preload).hide();
+	for (var i = 0; i < words[0].length; i++) {
+		if ((words[0])[i] == ' ') {
+			continue;
+		} else {
+			numCharacters++;
+
+			character = document.createElement("img");
+			character.src = imageLocation + (words[0])[i] + fileFormat;
+			$(preload).append(character);
+
+			$(character).on("load", function () {
+				charactersLoaded++;
+				console.log("loaded image");
+				if (charactersLoaded >= numCharacters) {
+					console.log("all images loaded");
+					beginAnagrammer();
+				}
+			});
+		}
 	}
+
+	$(preload).ready(function () {
+		// Pre arrange to prevent weird pop in
+		arrangeAsString(words[1], 1);
+		arrangeAsString(words[0], 1);
+	})
 }
 
-$(preload).ready(function() {
-	arrangeAsString(words[1], 1);
-	arrangeAsString(words[0], 1);
+function beginAnagrammer() {
 	setTimeout(rearrangeLoop, arrangeRate - animationDuration);
-});
 
-function getRandomInt (min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function randomArrange (animateCounter) {
-	setTimeout(function() {arrangeAsString(words[2], animationDuration)}, arrangeRate * animateCounter);
-	return true;
+	if (typeof anagramLoaded == "function") {
+		anagramLoaded();
+	}
 }
